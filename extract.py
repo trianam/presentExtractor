1c48b1fbd0ab8d75d5373b5608c0a7ca42a56d4a
Stefano Martina <stefano.martina@unifi.it>
2024-11-23 12:39:50 +0100

    Add debug statistics analysis and corrected randomization problem.

--- extract.py
+++ extract.py
@@ -6,8 +6,11 @@ import random
 import smtplib
 from email.mime.text import MIMEText
 
+debugRep = 100000
+debug = False
+# debug = True
 data = json.load(open("data.json", 'rt'))
-users = json.load(open("users.json", 'rt'))
+users = json.load(open("usersScandicci.json", 'rt'))
 
 def sendMessage(dest, objective, debug=False):
     if debug:
@@ -30,23 +33,44 @@ print("Possibilities:")
 print(df.to_string(na_rep='-'))
 print("")
 
+stats = dict()
+for name1 in users['mails'].keys():
+    stats[name1] = dict()
+    for name2 in users['mails'].keys():
+        stats[name1][name2] = 0
 
-result = dict()
-extracted = set()
+for rep in range(debugRep if debug else 1):
+    result = dict()
+    extracted = set()
 
-while len(result) == 0:
-    try:
-        for name in users['mails'].keys():
-            possibles = set([ k for k in users['possibilities'][name] if users['possibilities'][name][k] == True ])
-            result[name] = random.sample(list(possibles - extracted), 1)[0]
-            extracted.add(result[name])
-    except:
-        result = dict()
-        extracted = set()
+    while len(result) == 0:
+        try:
+            shuffledNames = list(users['mails'].keys())
+            random.shuffle(shuffledNames)
+            for name in shuffledNames:
+                possibles = set([ k for k in users['possibilities'][name] if users['possibilities'][name][k] == True ])
+                result[name] = random.sample(list(possibles - extracted), 1)[0]
+                extracted.add(result[name])
+        except:
+            result = dict()
+            extracted = set()
 
-for name in result.keys():
-    print("Send to {} ({})".format(name, users['mails'][name]))
-    sendMessage(name, result[name])
+    for name in result.keys():
+        if rep == 0:
+            print("Send to {} ({})".format(name, users['mails'][name]))
+            sendMessage(name, result[name], debug)
+
+        stats[name][result[name]] += 1
+
+
+if debug:
+    print(f"Frequencies over {debugRep}:")
+    for k1 in sorted(stats.keys()):
+        print(f"{k1} -> {[f'{k2}: {stats[k1][k2]}' for k2 in sorted(stats[k1].keys())]}")
+
+    print(f"Probabilities over {debugRep}:")
+    for k1 in sorted(stats.keys()):
+        print(f"{k1} -> {[f'{k2}: {stats[k1][k2]/debugRep*100:.1f}%' for k2 in sorted(stats[k1].keys())]}")
 
 print("\nExtraction process completed")
 
